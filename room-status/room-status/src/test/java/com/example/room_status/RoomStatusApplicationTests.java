c2175f628e06e5376e845b5504f4023ab17e4f80
lyes-hamici <lyes.hamici@laplateforme.io>
2026-03-09 14:41:10 +0100

    init : added spring initializr

---
+++ room-status/room-status/src/test/java/com/example/room_status/RoomStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.room_status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
