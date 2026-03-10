d125b7cf0643c0247dab3b6ba790deb2708179be
lyes-hamici <lyes.hamici@laplateforme.io>
2026-03-10 10:36:24 +0100

    feat : new file HeathController

--- room-status/room-status/src/main/java/com/example/roomstatus/controller/HealthController.java
+++ room-status/room-status/src/main/java/com/example/roomstatus/controller/HealthController.java
@@ -1,5 +1,21 @@
 package com.example.roomstatus.controller;
 
+import com.example.roomstatus.dto.response.HealthResponse;
+import com.example.roomstatus.service.HealthService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HealthController {
-    
+
+    private final HealthService healthService;
+
+    public HealthController(HealthService healthService) {
+        this.healthService = healthService;
+    }
+
+    @GetMapping("/api/v1/health")
+    public HealthResponse health() {
+        return healthService.getHealth();
+    }
 }
