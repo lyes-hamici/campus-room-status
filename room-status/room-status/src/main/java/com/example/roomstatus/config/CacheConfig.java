d460be24ed61849a500f88a2810ecec2c0b3a0d1
baptiste-appriou <baptiste.appriou@laplateforme.io>
2026-03-11 10:20:48 +0100

    feat: enhance CacheConfig to define cache manager for room-related entities

--- room-status/room-status/src/main/java/com/example/roomstatus/config/CacheConfig.java
+++ room-status/room-status/src/main/java/com/example/roomstatus/config/CacheConfig.java
@@ -1,5 +1,22 @@
 package com.example.roomstatus.config;
 
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class CacheConfig {
-    
+
+    @Bean
+    public CacheManager cacheManager() {
+        ConcurrentMapCacheManager cacheManager = new ConcurrentMapCacheManager(
+                "buildings",
+                "rooms",
+                "room-details",
+                "room-schedules"
+        );
+        cacheManager.setAllowNullValues(false);
+        return cacheManager;
+    }
 }
